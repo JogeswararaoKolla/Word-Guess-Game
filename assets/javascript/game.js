4e761022f666e5b499d9dc714218a0ae01d8b9c6
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-06-01 00:18:49 -0700

    working prototype need to add css and javascript objects code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,37 @@
 // Global Variable Declarations
-let currentWordList = ["apple", "oroangoes"];
-let currentWord;
+let currentWordList = ["apple", "oranges", "bananas", "lime"];
 let currentWordTmp = [];
-let i = 0;
-let GuessSoFar = [];
-let startIdx = 0;
-let lastIndex = 0;
+let i = 0,
+    startIdx = 0,
+    lastIndex = 0;
 let noOfGuess = 15;
+let randIdx = 0;
 let userKeyPress = [];
 let gameOverStats = false;
 let winStats = false;
-let winsCount=0;
-let temp=[];
+let winsCount = 0;
+let guessWord = '';
+let guessWordList = [];
 
 
-currentWordGet(currentWordList[0]);
-
-temp.length=currentWordList[0].length;
-
-console.log(temp.length);
-temp.fill('-');
-
-console.log(temp);
-
-
-function startWordGuessGame(){
-    GuessSoFar = [];
-
-}
-
-// console.log(currentWordList[0], currentWord);
-
-function currentWordGet(wordTmp) {
-
-    for (i = 0; i < wordTmp.length; i++) {
-        currentWordTmp.push("-");
-    }
-    console.log(currentWordTmp);
+function startWordGuessGame() {
+    userKeyPress = [];
+    winStats = false;
+    gameOverStats = false;
+    startIdx = 0;
+    lastIndex = 0;
+    noOfGuess = 15;
+    randIdx = Math.floor(Math.random() * currentWordList.length);
+    console.log(randIdx);
+    currentWordTmp.length = currentWordList[randIdx].length;
+    currentWordTmp.fill('-');
+    guessWord = currentWordList[randIdx];
+    guessWordList = Array.from(guessWord);
+    console.log(guessWordList);
     document.getElementById("current-word").innerText = currentWordTmp.join('');
-    return currentWordTmp.join('');
-
-}
-
-function removeDuplicatesArray(arr) {
-    let unique_array = []
-    for (let Idx = 0; Idx < arr.length; Idx++) {
-        if (unique_array.indexOf(arr[Idx]) == -1) {
-            unique_array.push(arr[Idx])
-        }
-    }
-    return unique_array
+    document.getElementById("noofguess").innerText = noOfGuess;
+    document.getElementById("letter-guessed").innerText = userKeyPress.join();
+    document.getElementById("wins").innerText = winsCount;
 }
 
 function arrayEqualsCheck(arr1, arr2) {
@@ -66,56 +48,56 @@ function arrayEqualsCheck(arr1, arr2) {
     winsCount++;
     document.getElementById("wins").innerText = winsCount;
     winStats = true;
+    gameOverStats = true;
     return winStats;
 }
 
-document.onkeyup = function (event) {
-
-    console.log(String.fromCharCode(event.keyCode).toLowerCase());
-    let user_key = String.fromCharCode(event.keyCode).toLowerCase();
-
-
-    if (userKeyPress.indexOf(user_key) == -1) {
-        userKeyPress.push(user_key);
-        noOfGuess--;
+function validateCharCode(charValue) {
+    if (!(charValue >= 65 && charValue <= 90)) {
+        console.log(charValue);
+        alert("Not a valid Character letter");
+        return false;
     }
-
-    console.log(noOfGuess);
-    console.log(userKeyPress);
-
-    let guessWord = currentWordList[0];
-    let guessWordList = guessWord.split('');
-    let guessWordListTmp = Array.from(guessWord);
-
-    console.log(guessWordListTmp);
-
-    //The Array.from() method creates a new, shallow-copied Array instance from an array-like or iterable object.
-    // console.log(guessWordList);
-    // console.log(guessWord.split(''));
-
-
-    if (guessWordList.includes(user_key)) {
-        startIdx = guessWordList.indexOf(user_key);
-        console.log("Start Index " + startIdx);
-        lastIndex = guessWordList.lastIndexOf(user_key);
-        console.log("last Index " + lastIndex);
-        GuessSoFar.push(user_key);
-        for (i = startIdx; i <= lastIndex; i++) {
-            if (guessWordList[i] == user_key) {
-                console.log("Matching Indexes are:", i);
-                currentWordTmp[i] = user_key;
-                console.log(currentWordTmp, currentWordTmp.length);
-            }
-            console.log(currentWordTmp.join(''), currentWordTmp.length);
-        }
-        document.getElementById("current-word").innerText = currentWordTmp.join('');
-        arrayEqualsCheck(currentWordTmp, guessWordListTmp);
-
-        console.log(currentWordTmp, guessWordListTmp);
-        console.log("Wins Stats : " + winStats);
+    else {
+        return true;
     }
-
 
 }
 
+startWordGuessGame();
 
+document.onkeyup = function (event) {
+
+    let user_key = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (validateCharCode(event.keyCode) && (userKeyPress.indexOf(user_key) == -1)) {
+        userKeyPress.push(user_key);
+        noOfGuess--;
+    }
+    document.getElementById("letter-guessed").innerText = userKeyPress.join();
+    document.getElementById("noofguess").innerText = noOfGuess;
+
+    if (noOfGuess == 0) {
+        gameOverStats = true;
+    }
+
+    if (!gameOverStats) {
+        if (guessWordList.includes(user_key)) {
+            startIdx = guessWordList.indexOf(user_key);
+            lastIndex = guessWordList.lastIndexOf(user_key);
+            for (i = startIdx; i <= lastIndex; i++) {
+                if (guessWordList[i] == user_key) {
+                    currentWordTmp[i] = user_key;
+                }
+            }
+            document.getElementById("current-word").innerText = currentWordTmp.join('');
+            arrayEqualsCheck(currentWordTmp, guessWordList);
+            console.log(currentWordTmp, guessWordList);
+        }
+
+    }
+    else {
+        startWordGuessGame();
+    }
+
+}
